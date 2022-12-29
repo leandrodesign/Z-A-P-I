91dcf415b8c609f817df01704c774ec517c6bbbc
leandro <atendimentoldhomestudio@gmail.com>
2022-12-29 12:02:56 -0300

    primeiro commit

---
+++ src/api/class/session.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-unsafe-optional-chaining */
+const { WhatsAppInstance } = require('../class/instance')
+const logger = require('pino')()
+const Chat = require('../models/chat.model')
+const config = require('../../config/config')
+
+class Session {
+    async restoreSessions() {
+        let restoredSessions = new Array()
+        let allCollections = []
+        try {
+            const db = mongoClient.db('whatsapp-api')
+            const result = await db.listCollections().toArray()
+            result.forEach((collection) => {
+                allCollections.push(collection.name)
+            })
+
+            allCollections.map(async (key) => {
+                const query = {}
+                db.collection(key)
+                    .find(query)
+                    .toArray(async (err, result) => {
+						  const chat = await Chat.findOne({ key: key })
+
+                        const data = {
+                            name: chat.key,
+                            webhook: chat.config.allowWebhook,
+                            webhookUrl: chat.config.customWebhook,
+                        }
+                        if (err) throw err
+                        const webhook = chat.config.allowWebhook
+                            ? chat.config.allowWebhook
+                            : config.webhookEnabled
+                        const webhookUrl = chat.config.customWebhook
+                            ? chat.config.customWebhook
+                            : config.webhookUrl
+                        const instance = new WhatsAppInstance(
+                            key,
+                            webhook,
+                            webhookUrl
+                        )
+                        await instance.init()
+                        WhatsAppInstances[key] = instance
+                    })
+                restoredSessions.push(key)
+            })
+        } catch (e) {
+            logger.error('Error restoring sessions')
+            logger.error(e)
+        }
+        return restoredSessions
+    }
+}
+
+exports.Session = Session
---
+++ src/api/models/chat.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const chatSchema = new mongoose.Schema({
+    key: {
+        type: String,
+        required: [true, 'key is missing'],
+        unique: true,
+    },
+	config: {
+        type: Object,
+        required: [true, 'key is missing'],
+    },
+    chat: {
+        type: Array,
+    },
+})
+
+const Chat = mongoose.model('Chat', chatSchema)
+
+module.exports = Chat
